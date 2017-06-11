f93a503a44cf8a8f3435aa267a6c88e07bbacaae
YU SUN <chinaysun@gmail.com>
2017-06-11 22:43:35 +1000

    Add files via upload
    1. Learnt how to use core location
    2. Create an annotation into map

--- MKLearning/MKLearning/ViewController.swift
+++ MKLearning/MKLearning/ViewController.swift
@@ -15,6 +15,10 @@ class ViewController: UIViewController,CLLocationManagerDelegate {
     
     let locationManager = CLLocationManager()
     
+    //default location for tester
+    let myLatitude:CLLocationDegrees = -37.8009135
+    let myLongtitude:CLLocationDegrees =  144.95518489999995
+    
     @IBOutlet weak var myMap: MKMapView!
     
     
@@ -24,7 +28,7 @@ class ViewController: UIViewController,CLLocationManagerDelegate {
         let location = locations.last!
     
         
-        let span:MKCoordinateSpan = MKCoordinateSpanMake(0.07,0.07)
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(0.001,0.001)
         let myLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
         let region:MKCoordinateRegion = MKCoordinateRegionMake(myLocation, span)
         
@@ -42,10 +46,74 @@ class ViewController: UIViewController,CLLocationManagerDelegate {
     
     @IBAction func fixButtonTapped(_ sender: Any) {
         
+        //request once
         locationManager.requestLocation()
+        
+    }
+    
+    @IBAction func letGoButtonTapped(_ sender: Any) {
+        
+      
+                
+        let myCoordinator = CLLocationCoordinate2DMake(myLatitude, myLongtitude)
+                
+       myMap.camera.centerCoordinate = myCoordinator
+    
+        
+     
+    }
+    
+    
+    @IBAction func addRemovePinTapped(_ sender: UIButton) {
+        
+        //my First Pin
+        let myFirstPin = MKPointAnnotation()
+        
+        myFirstPin.title = "This is my First Pin"
+        myFirstPin.subtitle = "Please visit me"
+        myFirstPin.coordinate = CLLocationCoordinate2DMake(-37.800565928293615, 144.95576425714717)
+        
+        
+        //check if it exist or not
+        var pinExist = false
+        
+        for items in myMap.annotations
+        {
+            if items.coordinate.latitude == myFirstPin.coordinate.latitude && items.coordinate.longitude == myFirstPin.coordinate.longitude
+            {
+                pinExist = true
+                break
+            }
+            
+        }
+        
+        if !pinExist {
+            myMap.addAnnotation(myFirstPin)
+            sender.setTitle("Remove a pin from map", for: UIControlState.normal)
+        }else
+        {
+            for items in myMap.annotations
+            {
+                if items.coordinate.latitude == myFirstPin.coordinate.latitude && items.coordinate.longitude == myFirstPin.coordinate.longitude
+                {
+                    myMap.removeAnnotation(items)
+                    break
+                }
+            }
+            
+            sender.setTitle("Add a pin into map", for: UIControlState.normal)
+            
+        }
+        
         
         
     }
+    
+
+    
+    
+    
+    
    
     
     override func viewDidLoad() {
