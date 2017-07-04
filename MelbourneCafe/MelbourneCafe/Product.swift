fffbbdb07633650eb65424a0a835161b7dfe5d91
YU SUN <chinaysun@gmail.com>
2017-07-04 21:00:28 +1000

    General Class For Cafe Product
    1. People can add more features in the furture

---
+++ MelbourneCafe/MelbourneCafe/Product.swift
@@ -0,0 +1,26 @@
+//
+//  Product.swift
+//  MelbourneCafe
+//
+//  Created by SUN YU on 4/7/17.
+//  Copyright © 2017 SUN YU. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Product
+{
+    var productID:Int = 0
+    var name:String = ""
+    var imageURL:String = ""
+    var productImage:UIImage!
+    {
+        get
+        {
+            return (UIImage(named: "defaultProfileImage"))!
+        }
+    }
+    var price: Double = 0.0
+    
+}
