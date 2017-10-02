24989684874056c25894b542f5962dbb69be5e71
Yu SUN MacPro <ausysun@gmail.com>
2017-10-02 14:33:49 +0800

    Share the image view

--- CollageShare/CollageShare/ViewController.swift
+++ CollageShare/CollageShare/ViewController.swift
@@ -51,9 +51,27 @@ class ViewController: UIViewController,UIDropInteractionDelegate,UIDragInteracti
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor.white
+        
+        navigationItem.title = "Collage Sharing"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Share", style: .plain, target: self, action: #selector(handleShare))
         
         view.addInteraction(UIDropInteraction(delegate: self))
         view.addInteraction(UIDragInteraction(delegate: self))
+    }
+    
+    @objc func handleShare()
+    {
+        UIGraphicsBeginImageContext(view.frame.size)
+        view.layer.render(in: UIGraphicsGetCurrentContext()!)
+        guard let image = UIGraphicsGetImageFromCurrentImageContext() else { return }
+        UIGraphicsEndImageContext()
+        
+        let activityViewController = UIActivityViewController(activityItems: [image,"Sharing this nice image"], applicationActivities: nil)
+        
+        activityViewController.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        
+        present(activityViewController, animated: true, completion: nil)
     }
     
     func dropInteraction(_ interaction: UIDropInteraction, performDrop session: UIDropSession) {
@@ -73,6 +91,10 @@ class ViewController: UIViewController,UIDropInteractionDelegate,UIDragInteracti
                 DispatchQueue.main.async {
                     let imageView = UIImageView(image: draggedImage)
                     imageView.isUserInteractionEnabled = true
+                    imageView.layer.borderWidth = 4
+                    imageView.layer.borderColor = UIColor.black.cgColor
+                    imageView.layer.shadowRadius = 5
+                    imageView.layer.shadowOpacity = 0.3
                     self.view.addSubview(imageView)
                     imageView.frame = CGRect(x: 0, y: 0, width: draggedImage.size.width, height: draggedImage.size.height)
                     
